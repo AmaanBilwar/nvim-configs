50cc98fb302eb3d457ba968ee2132bff70809c9b
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-02-06 23:30:23 -0500

    add tree dir structure for faster navigation

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,7 +2,18 @@ return {
 	"nvim-tree/nvim-tree.lua",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		local api = require("nvim-tree.api")
+
+		local function on_attach(bufnr)
+			api.config.mappings.default_on_attach(bufnr)
+			vim.keymap.set("n", "s", api.node.open.horizontal, { buffer = bufnr, desc = "Open: Horizontal Split" })
+			vim.keymap.set("n", "v", api.node.open.vertical, { buffer = bufnr, desc = "Open: Vertical Split" })
+			vim.keymap.set("n", "<C-w>s", api.node.open.horizontal, { buffer = bufnr, desc = "Open: Horizontal Split" })
+			vim.keymap.set("n", "<C-w>v", api.node.open.vertical, { buffer = bufnr, desc = "Open: Vertical Split" })
+		end
+
 		require("nvim-tree").setup({
+			on_attach = on_attach,
 			view = {
 				width = 30,
 				side = "left",
