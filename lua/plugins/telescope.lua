6c6df004be6076a133dc26806df054d34b5df0b2
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-02-28 04:39:39 -0600

    update telescope  to show hidden files & exlude some dirs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,7 +21,24 @@ return {
 			},
 		},
 	})
-	vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+	vim.keymap.set('n', '<leader>ff', function()
+		builtin.find_files({
+			hidden = true,
+			no_ignore = true,
+			find_command = {
+				'rg',
+				'--files',
+				'--hidden',
+				'--no-ignore',
+				'--glob',
+				'!.git/*',
+				'--glob',
+				'!node_modules/*',
+				'!target/*',
+				'.helix/*'
+			},
+		})
+	end, { desc = 'Telescope find files (all)' })
 	vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 	vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 	vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
