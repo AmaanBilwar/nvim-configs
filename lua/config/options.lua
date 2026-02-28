5bdd4fc604fa3a31e774bd12a8dac2237826503c
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-02-28 10:47:17 -0600

    add clipboard settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,13 +2,6 @@ vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
-
+vim.opt.clipboard = "unnamedplus"
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-
-
-
-
-
-
-
