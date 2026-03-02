43c6a9626476d3938a8054af0abf5d89c119c145
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-03-02 14:59:37 -0600

    lazy load lazygit

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,6 +1,7 @@
 return {
   "kdheepak/lazygit.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
+  lazy = false,
   config = function()
     vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "Open lazygit" })
   end,
