ac3f16776096420a2860d204fa1efc1b2ae32b61
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-04-10 16:23:55 +0530

    add fff

---
+++ lua/plugins/fff.lua
@@ -0,0 +1,19 @@
+return {
+    'dmtrKovalenko/fff.nvim',
+    build = function()
+	require('fff.download').download_or_build_binary()
+    end,
+    lazy = false,
+    keys = {
+	{
+	    '<leader>ff',
+	    function() require('fff').find_files() end,
+	    desc = 'FFF find files',
+	},
+	{
+	    '<leader>fg',
+	    function() require('fff').live_grep() end,
+	    desc = 'FFF live grep',
+	},
+    },
+}
