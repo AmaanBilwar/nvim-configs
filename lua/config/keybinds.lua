4c10a5c2a2813bcaa63ac3faa54db817b2b20e06
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-04-16 00:23:47 +0530

    add markdown rendering plugin

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", "<cmd>Oil<cr>")
+vim.keymap.set("n", "<leader>.", function()
+  require("oil").toggle_hidden()
+end, { desc = "Oil: toggle hidden files" })
---
+++ lua/plugins/render_markdown.lua
@@ -0,0 +1,9 @@
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {},
+---@module 'render-markdown'
+---@type render.md.UserConfig
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    lazy = false,
+}
+
