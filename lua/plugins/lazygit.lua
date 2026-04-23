73b42ee98ff421c14c4c6cb7ef9b2c00c04add6d
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-04-24 00:21:40 +0530

    remove other configs

--- lua/plugins/lazygit.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "kdheepak/lazygit.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  lazy = false,
-  config = function()
-    vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<cr>", { desc = "Open lazygit" })
-  end,
-}
--- lua/plugins/telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-	local builtin = require('telescope.builtin')
-	vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-	vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    end
-}
