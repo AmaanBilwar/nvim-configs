05d7068e8ed357b1d8afe47db3017e4238eaba9f
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-04-24 00:21:40 +0530

    remove other configs

--- lua/config/keybinds.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>cd", "<cmd>Oil<cr>", { desc = "Open oil" })
-vim.keymap.set("n", "<S-h>", function()
-  require("oil").toggle_hidden()
-end, { desc = "Oil: toggle hidden files" })
--- lua/plugins/render_markdown.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "MeanderingProgrammer/render-markdown.nvim",
-    opts = {},
----@module 'render-markdown'
----@type render.md.UserConfig
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-    lazy = false,
-}
-
